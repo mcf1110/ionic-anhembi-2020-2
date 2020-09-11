3b348edbf0fb8dab4848f8092c5dc6ec303ab64e
Matheus Fernandes <mcfernandes1110@gmail.com>
2020-09-10 21:11:32 -0300

    nubank++

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -8,18 +8,22 @@ import { Component } from '@angular/core';
 export class HomePage {
   public currentValue = 0;
   public maxValue = 0;
+  public selectedCash = 0;
 
   public increment() {
-    this.currentValue++;
+    this.currentValue += this.selectedCash;
     if (this.currentValue > this.maxValue) {
       this.maxValue = this.currentValue;
     }
+    this.selectedCash = 0;
   }
 
   public decrement() {
-    if (this.currentValue > 0) {
-      this.currentValue--;
+    this.currentValue -= this.selectedCash;
+    if (this.currentValue < 0) {
+      this.currentValue = 0;
     }
+    this.selectedCash = 0;
   }
 
   public reset() {
