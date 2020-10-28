b652c8a18547070c8a23ac6063a5feec46399a96
Matheus Fernandes <mcfernandes1110@gmail.com>
2020-10-28 20:42:37 -0300

    versao basica do plugin de network

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,4 +20,4 @@ import { AppRoutingModule } from './app-routing.module';
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { Plugins } from '@capacitor/core'
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -7,6 +9,18 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor() {
+  }
+
+  public async getPhoto() {
+    const connection = await Plugins.Network.getStatus();
+    if (connection.connectionType === 'wifi') {
+      console.log('Você está no wifi!');
+    } else if (connection.connectionType === 'none') {
+      console.log('Você está sem internet!');
+    } else {
+      console.log('Você está com um 3G ou algo do tipo!');
+    }
+  }
 
 }
