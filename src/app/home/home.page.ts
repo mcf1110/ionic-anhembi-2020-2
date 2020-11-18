54ac6373b704bbe68b9a6ae9fd672ce718dffa23
Matheus Fernandes <mcfernandes1110@gmail.com>
2020-11-18 20:05:40 -0300

    carregamento na pagina de artistas

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -10,13 +10,16 @@ import { ArtistResult } from '../types/artist.result';
 export class HomePage {
   public search = '';
   public result: ArtistResult = null;
+  public isLoading = false;
   constructor(private musicService: MusicService) { }
 
   public async makeSearch() {
     if (this.search.length < 2) {
       return;
     }
+    this.isLoading = true;
     this.result = await this.musicService.searchArtist(this.search);
+    this.isLoading = false;
   }
 
 }
