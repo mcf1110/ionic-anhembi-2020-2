53540c939f9344a99385a0a63d165afea5676fcc
Matheus Fernandes <mcfernandes1110@gmail.com>
2020-11-19 19:53:03 -0300

    removendo http

--- src/app/services/music.service.ts
+++ src/app/services/music.service.ts
@@ -11,12 +11,12 @@ export class MusicService {
   constructor(private http: HttpClient) { }
 
   public async searchArtist(artist: string) {
-    const url = "http://musicbrainz.org/ws/2/artist/?query=" + artist + "&fmt=json"
+    const url = "//musicbrainz.org/ws/2/artist/?query=" + artist + "&fmt=json"
     return await this.http.get(url).toPromise() as ArtistResult;
   }
 
   public async getReleases(id: string) {
-    const url = `http://musicbrainz.org/ws/2/artist/${id}?inc=release-groups&fmt=json`
+    const url = `//musicbrainz.org/ws/2/artist/${id}?inc=release-groups&fmt=json`
     return await this.http.get(url).toPromise() as ReleaseResult;
   }
 }
