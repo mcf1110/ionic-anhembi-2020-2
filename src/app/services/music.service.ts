9606bc54e1bfd5af90c71bbc08e07b233a3882bf
Matheus Fernandes <mcfernandes1110@gmail.com>
2020-11-23 19:21:27 -0300

    Corrigindo mixed content

--- src/app/services/music.service.ts
+++ src/app/services/music.service.ts
@@ -11,12 +11,12 @@ export class MusicService {
   constructor(private http: HttpClient) { }
 
   public searchArtists(searchTerm: string) {
-    const url = `http://musicbrainz.org/ws/2/artist/?query=${searchTerm}&fmt=json`;
+    const url = `//musicbrainz.org/ws/2/artist/?query=${searchTerm}&fmt=json`;
     return this.http.get(url).toPromise() as Promise<SearchArtistsResult>;
   }
 
   public getAlbums(id: string) {
-    const url = `http://musicbrainz.org/ws/2/artist/${id}?inc=release-groups&fmt=json`;
+    const url = `//musicbrainz.org/ws/2/artist/${id}?inc=release-groups&fmt=json`;
     return this.http.get(url).toPromise() as Promise<GetAlbumsResult>;
 
   }
